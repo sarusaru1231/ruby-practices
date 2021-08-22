fe99b3664479fbe54bcf650863be9376f188f420
sarusaru1231 <sarusaru1231@igaigaiga.com>
2021-08-23 06:21:49 +0900

    [fix] bowling.rb のコードを修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -9,71 +9,41 @@ end
 
 def score_calculate(scores)
   frames = convert_to_frames(scores)
-  point = 0
-  two_times_before = :normal
-  one_time_before = :normal
-  frames.each_with_index do |frame, frame_count|
-    point, two_times_before = two_times_before_score_calculate(point, two_times_before, frame)
-    point, two_times_before = one_time_before_score_calculate(point, one_time_before, frame)
-    point, one_time_before = this_time_score_calculate(point, frame_count, frame)
+  total_point = 0
+  frames.each_with_index do |this_frame, frame_number|
+    total_point += this_frame.sum
+    total_point += frames[frame_number + 2][0] if frame_number < 8 && (strike?(this_frame) && strike?(frames[frame_number + 1]))
+    if frame_number < 9
+      total_point += frames[frame_number + 1][0] + frames[frame_number + 1][1] if strike?(this_frame)
+      total_point += frames[frame_number + 1][0] if spare?(this_frame)
+    end
   end
-  point
+  total_point
+end
+
+def strike?(frame)
+  frame[0] == 10
+end
+
+def spare?(frame)
+  frame[0] < 10 && frame.sum == 10
 end
 
 def convert_to_frames(scores)
   shots = []
-  scores.each do |s|
-    if s == 'X'
+  scores.each do |shot|
+    if shot == 'X'
       shots << 10
-      shots << 0
+      shots << 0 if shots.size < 18
     else
-      shots << s.to_i
+      shots << shot.to_i
     end
   end
   frames = []
-  shots.each_slice(2) do |s|
-    s.length == 1 ? frames.last << s[0] : frames << s
+  shots.each_slice(2) do |shot|
+    shot.length == 1 ? frames.last << shot[0] : frames << shot
   end
   frames
-end
-
-def one_time_before_score_calculate(point, one_time_before, frame)
-  case one_time_before
-  when :strike
-    if frame[0] == 10
-      two_times_before = :strike
-      point += 10
-    else
-      point += frame[0] + frame[1]
-    end
-  when :spare
-    point += frame[0]
-  end
-  [point, two_times_before]
-end
-
-def two_times_before_score_calculate(point, two_times_before, frame)
-  if two_times_before == :strike
-    point += frame[0]
-    two_times_before = :normal
-  end
-  [point, two_times_before]
-end
-
-def this_time_score_calculate(point, frame_count, frame)
-  one_time_before = :normal
-  if frame_count >= 9 # frame10
-    point += frame.sum
-  elsif frame[0] == 10 # strike
-    point += 10
-    one_time_before = :strike
-  elsif frame.sum == 10 # spare
-    point += frame.sum
-    one_time_before = :spare
-  else
-    point += frame.sum
-  end
-  [point, one_time_before]
 end
 
 main
